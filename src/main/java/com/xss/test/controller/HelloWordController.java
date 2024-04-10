d6be09d3629835fdf22bfd3c7ebefc67e85c115f
xu457 <457683048@qq.com>
2024-04-10 17:36:06 +0800

    测试

--- src/main/java/com/xss/test/controller/HelloWordController.java
+++ src/main/java/com/xss/test/controller/HelloWordController.java
@@ -3,11 +3,13 @@ package com.xss.test.controller;
 import com.xss.test.services.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 测试类
  */
+@RequestMapping("api/")
 @RestController
 public class HelloWordController {
     @Autowired
@@ -16,7 +18,7 @@ public class HelloWordController {
      * 验证mybatis的批处理
      * @return 、、
      */
-    @GetMapping(value = "/test")
+    @GetMapping(value = "test")
     public Object testBatchInsert(){
         long a = System.currentTimeMillis();
         helloWorldService.batchInsertUsers();
@@ -29,7 +31,7 @@ public class HelloWordController {
      * 验证mybatis的批处理
      * @return 、、
      */
-    @GetMapping(value = "/testAop")
+    @GetMapping(value = "testAop")
     public Object testAop(){
         System.out.println("testAop");
 
--- src/main/java/com/xss/test/controller/TestTemplatesController.java
+++ src/main/java/com/xss/test/controller/TestTemplatesController.java
@@ -2,11 +2,11 @@ package com.xss.test.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RequestMapping("api/")
 @Controller
 public class TestTemplatesController {
 
-    @RequestMapping(value = "/getUser")
+    @RequestMapping(value = "getUser")
     public String getUser(){
         return "user";
     }
